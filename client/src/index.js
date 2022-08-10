577b7663ddb29da549061c0b5293d5ff33d5897b
Julian Quan Fun <julianquanfun@gmail.com>
2022-08-10 18:19:00 -0400

    heroku again

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-    <>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-    </>
-  // </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
